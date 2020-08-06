17bc281796b899a04f15b129a57c9e8c2dadb0dc
18016375 <cecilialui16@yahoo.com.sg>
2020-08-06 13:19:21 +0800

    activity 1 - 1000000000000

--- app/src/main/java/sg/edu/rp/webservices/demonavigation/NewActivity.java
+++ app/src/main/java/sg/edu/rp/webservices/demonavigation/NewActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NewActivity extends AppCompatActivity {
 
@@ -16,6 +19,16 @@ public class NewActivity extends AppCompatActivity {
 
         ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //action
+            }
+        });
+
     }
 
 }
